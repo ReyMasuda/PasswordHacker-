51fa95545669076eb7b50e7e0ad8dc14036c38e3
Rey <quit9909@icloud.com>
2015-03-07 23:24:26 +0900

    ちょいと機能をつけたんだぜ。 昨日はでずねー行ってて疲れて寝ちまったぜ。 今日からまた毎日や！

--- PasswordHacker/ViewController.h
+++ PasswordHacker/ViewController.h
@@ -8,7 +8,17 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    IBOutlet UILabel *resultLabe1; //結果(一番右のラベル)
+    IBOutlet UILabel *resultLabe2;//結果(一番右から2番目のラベル)
+    IBOutlet UILabel *resultLabe3;//結果(一番右から3番目のラベル)
+    IBOutlet UILabel *resultLabe4;//結果(一番右から4番目のラベル)
+    IBOutlet UILabel *countLabel;//結果カウント中の数字を表すラベル
+    
+    int password; //探し出す数字
+}
+
+-(IBAction)start;
 
 
 @end
--- PasswordHacker/ViewController.m
+++ PasswordHacker/ViewController.m
@@ -17,11 +17,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    resultLabe1.text = @"?";
+    resultLabe2.text = @"?";
+    resultLabe3.text = @"?";
+    resultLabe4.text = @"?";
+    
+    password = 1234;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)start{
+    for (int i = 0 ; i <10000 ; i = i +1) {
+        countLabel.text = [NSString stringWithFormat:@"%d",i]; //カウント中の数字をcountLabelに表示する
+        
+        [[NSRunLoop currentRunLoop]
+         runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.0005]
+         ]; //処理を0.0005秒、待機する
+    }
 }
 
 @end
