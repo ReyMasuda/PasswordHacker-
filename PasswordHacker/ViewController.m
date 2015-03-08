b66e8691f5ed4e7484af5e6189a1fd37235dbec6
Rey <quit9909@icloud.com>
2015-03-08 23:20:24 +0900

    エラーがどうしても治らない！！！！！！！

--- PasswordHacker/ViewController.m
+++ PasswordHacker/ViewController.m
@@ -32,13 +32,28 @@
 }
 
 -(IBAction)start{
-    for (int i = 0 ; i <10000 ; i = i +1) {
+    for (int i = 0 ; i < 10000 ; i = i +1) {
         countLabel.text = [NSString stringWithFormat:@"%d",i]; //カウント中の数字をcountLabelに表示する
         
         [[NSRunLoop currentRunLoop]
          runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.0005]
          ]; //処理を0.0005秒、待機する
     }
+    
+    if(i == password){ //passwordと一致した場合
+        int digit[4]; //passwordの桁ごとの数字
+        
+        for (int j = 0; j < 4 ; j = j + 1) {
+            digit[j] = (password % 10); //j桁目の数字
+            password = password / 10;
+        }
+        
+        resultLabe1.text = [NSString stringWithFormat:@"%d",digit[0]];
+        resultLabe2.text = [NSString stringWithFormat:@"%d",digit[1]];
+        resultLabe3.text = [NSString stringWithFormat:@"%d",digit[2]];
+        resultLabe4.text = [NSString stringWithFormat:@"%d",digit[3]];
+}
+    
 }
 
 @end
