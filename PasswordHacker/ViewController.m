0bc7a793340ad8ccb8f585baa6abdcefcdd34e39
Rey <quit9909@icloud.com>
2015-03-09 00:19:20 +0900

    綿貫ちゃんありがとだぜ オバかしてたぜ

--- PasswordHacker/ViewController.m
+++ PasswordHacker/ViewController.m
@@ -38,22 +38,24 @@
         [[NSRunLoop currentRunLoop]
          runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.0005]
          ]; //処理を0.0005秒、待機する
+        
+        if(i == password){ //passwordと一致した場合
+            int digit[4]; //passwordの桁ごとの数字
+            
+            for (int j = 0; j < 4 ; j = j + 1) {
+                digit[j] = (password % 10); //j桁目の数字
+                password = password / 10;
+            }
+            
+            resultLabe1.text = [NSString stringWithFormat:@"%d",digit[0]];
+            resultLabe2.text = [NSString stringWithFormat:@"%d",digit[1]];
+            resultLabe3.text = [NSString stringWithFormat:@"%d",digit[2]];
+            resultLabe4.text = [NSString stringWithFormat:@"%d",digit[3]];
+
     }
-    
-    if(i == password){ //passwordと一致した場合
-        int digit[4]; //passwordの桁ごとの数字
         
-        for (int j = 0; j < 4 ; j = j + 1) {
-            digit[j] = (password % 10); //j桁目の数字
-            password = password / 10;
-        }
-        
-        resultLabe1.text = [NSString stringWithFormat:@"%d",digit[0]];
-        resultLabe2.text = [NSString stringWithFormat:@"%d",digit[1]];
-        resultLabe3.text = [NSString stringWithFormat:@"%d",digit[2]];
-        resultLabe4.text = [NSString stringWithFormat:@"%d",digit[3]];
 }
-    
+
 }
 
 @end
